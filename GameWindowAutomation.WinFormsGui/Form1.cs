493da77993ed2af804aa6de967b5ef86385c9bc6
coelacanth16 <chris.moler@gmail.com>
2017-09-13 22:41:10 -0400

    adding in necessary libs

--- GameWindowAutomation.WinFormsGui/Form1.cs
+++ GameWindowAutomation.WinFormsGui/Form1.cs
@@ -8,35 +8,35 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using GameWindowAutomation.Core;
-using KeyboardListener;
 
 namespace GameWindowAutomation.WinFormsGui
 {
     public partial class Form1 : Form
     {
-        public static KeyboardListener.KeyboardListener keyListener = new KeyboardListener.KeyboardListener();
+        //public static KeyboardListener.KeyboardListener keyListener = new KeyboardListener.KeyboardListener();
+        IProgress<string> textBoxProgress;    
 
         public Form1()
         {
             InitializeComponent();
-            keyListener.AllKeys = false;
+            //keyListener.AllKeys = false;
             //keyListener.AddKeycode(Keycode.VK_CONTROL);
             //keyListener.AddKeycode(Keycode.VK_SHIFT);
             //keyListener.AddKeycode(Keycode.VK_ALT);
             //keyListener.AddKeycode(Keycode.VK_C);
-            keyListener.AddKeyCombo(new[] { Keycode.VK_CONTROL, Keycode.VK_SHIFT, Keycode.VK_C }).KeyComboPressed += CaptureWindowComboPressed;
-            keyListener.AddKeyCombo(new[] { Keycode.VK_CONTROL, Keycode.VK_ALT, Keycode.VK_C }).KeyComboPressed += CaptureStateComboPressed;
-            keyListener.KeyPressed += KeyListener_KeyPressed;
+            //keyListener.AddKeyCombo(new[] { Keycode.VK_CONTROL, Keycode.VK_SHIFT, Keycode.VK_C }).KeyComboPressed += CaptureWindowComboPressed;
+            //keyListener.AddKeyCombo(new[] { Keycode.VK_CONTROL, Keycode.VK_ALT, Keycode.VK_C }).KeyComboPressed += CaptureStateComboPressed;
+            //keyListener.KeyPressed += KeyListener_KeyPressed;
             //keyListener.KeyComboPressed += KeyListener_KeyComboPressed;
-            keyListener.Listen();
+            //keyListener.Listen();
             //KeyPressedStateGrid.SelectedObject = CurrentKeyState;
-            textBoxProgress = new Progress<string>((str) => keyPressedLogTB.AppendText(str + Environment.NewLine));            
-            keyListener.KeyPressed += keycode => textBoxProgress?.Report(keycode.ToString());
+            textBoxProgress = new Progress<string>((str) => keyPressedLogTB.AppendText(str + Environment.NewLine));
+            //keyListener.KeyPressed += keycode => textBoxProgress?.Report(keycode.ToString());
         }
 
-        private void CaptureWindowComboPressed(Keycode[] keycodes)
+        private void CaptureWindowComboPressed(int[] keycodes)
         {
-            KeyComboPressed(keycodes);
+            //KeyComboPressed(keycodes);
             Point point = new Point();
             if (Utilities.GetCursorPos(ref point))
             {
@@ -60,33 +60,32 @@ namespace GameWindowAutomation.WinFormsGui
                 //});
         }
 
-        private void CaptureStateComboPressed(Keycode[] keycodes)
-        {
-            KeyComboPressed(keycodes);
-        }
+        //private void CaptureStateComboPressed(Keycode[] keycodes)
+        //{
+        //    KeyComboPressed(keycodes);
+        //}
 
 
-        private void KeyComboPressed(Keycode[] keyCombo)
-        {
-            textBoxProgress?.Report(string.Join("+", keyCombo.Select(keycode => keycode.ToString().Replace("VK_", string.Empty))));
-        }
+        //private void KeyComboPressed(Keycode[] keyCombo)
+        //{
+        //    textBoxProgress?.Report(string.Join("+", keyCombo.Select(keycode => keycode.ToString().Replace("VK_", string.Empty))));
+        //}
 
-        IProgress<string> textBoxProgress;    
 
-        private void KeyListener_KeyPressed(Keycode keycode)
-        {
-            textBoxProgress?.Report(keycode.ToString().Replace("VK_", string.Empty));
-        }
+        //private void KeyListener_KeyPressed(Keycode keycode)
+        //{
+        //    textBoxProgress?.Report(keycode.ToString().Replace("VK_", string.Empty));
+        //}
 
-        private static async Task CaptureWindow()
-        {
+        //private static async Task CaptureWindow()
+        //{
 
-        }
+        //}
 
-        private static async Task CaptureState()
-        {
+        //private static async Task CaptureState()
+        //{
 
-        }
+        //}
 
         private void timer1_Tick(object sender, EventArgs e)
         {
